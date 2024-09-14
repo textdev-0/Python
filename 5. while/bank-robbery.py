b96dcede7201800dce9b06e7fe0948a915d875c2
maximv-25 <148719851+maximv-25@users.noreply.github.com>
2024-09-14 08:56:33 -0700

    Changes to be committed: 	new file:   5. while/bank-robbery.py

---
+++ 5. while/bank-robbery.py
@@ -0,0 +1,15 @@
+start_sum = float(input('Current sum >> '))
+target_sum = float(input('Target sum >> '))
+intrest = float(input('Monthly intrest rate >> '))
+final_sum = 0
+
+print()
+print('Testing...')
+
+while start_sum < target_sum:
+    final_sum += 1
+    start_sum += start_sum/100*intrest
+    print(f'Months: {final_sum}\nCurrent sum: {start_sum:0.2f}')
+
+print()
+print(f'Months taken: {final_sum}\nFinal sum: {start_sum:0.2f}')
